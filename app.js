d399d5320180d6481ac88156a30465ebc9d7c660
Paweł Jurek <pawel80121@gmail.com>
2024-05-07 10:25:57 +0200

    Dashboard

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/dashboard'));
 
 app.get('*', function(req, res){
     //res.status(404).send('404 Page not found :(');

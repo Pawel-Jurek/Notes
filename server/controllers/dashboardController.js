a11191ba128bc6e385082f24110db9ba241a5b50
Paweł Jurek <pawel80121@gmail.com>
2024-05-09 21:06:38 +0200

    displaying notes

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,10 +1,43 @@
+const Note = require('../models/Notes');
+const mongoose = require('mongoose');
+
 exports.dashboard = async (req, res) => {
+    let perPage = 8;
+    let page = req.query.page || 1;
+    
     const locals = {
         title: "Dashboard",
         description: "Node.js app"
     }
-    res.render('dashboard/index', {
-        locals,
-        layout: '../views/layouts/dashboard'
-    });
+    try {
+        const notes = await Note.aggregate([
+          { $sort: { updatedAt: -1 } },
+          { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+          {
+            $project: {
+              title: { $substr: ["$title", 0, 30] },
+              body: { $substr: ["$body", 0, 100] },
+            },
+          }
+          ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec(); 
+    
+        const count = await Note.countDocuments().exec();
+    
+        res.render('dashboard/index', {
+          username: req.user.firstName,
+          locals,
+          notes,
+          layout: "../views/layouts/dashboard",
+          current: page,
+          pages: Math.ceil(count / perPage)
+        });
+        
+ 
+    } catch(error) {
+        console.log(error);
+    }
+    
 }

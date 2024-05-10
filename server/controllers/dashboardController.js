8c1fa0c8127b99ac811c18f32ac6f4eae1093105
Paweł Jurek <pawel80121@gmail.com>
2024-05-10 12:51:22 +0200

    note view

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -41,3 +41,22 @@ exports.dashboard = async (req, res) => {
     }
     
 }
+
+exports.viewNote = async(req, res) => {
+    const note = await Note.findById(req.params.id)
+    .where({user: req.user.id}).lean();
+
+    if (note) {
+        res.render('dashboard/view-note', {
+            noteID: req.params.id,
+            note,
+            layout: '../views/layouts/dashboard'
+        });
+    } else {
+        res.send("Something went wrong")
+    }
+}
+
+exports.updateNote = async(req, res) => {
+    
+}
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,5 +4,7 @@ const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboardController = require('../controllers/dashboardController');
 
 router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
+router.get('/dashboard/item/:id', isLoggedIn, dashboardController.viewNote);
+router.post('/dashboard/item/:id', isLoggedIn, dashboardController.updateNote);
 
 module.exports = router;
